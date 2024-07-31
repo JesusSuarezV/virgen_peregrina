31e9c4408376e9a852ab191f9d1deae778629ca6
unknown <jesusalexandersv@ufps.edu.co>
2024-07-31 00:25:42 -0500

    Correccion de vistas y de js

--- src/main/resources/static/js/file.js
+++ src/main/resources/static/js/file.js
@@ -6,7 +6,22 @@ document.addEventListener('DOMContentLoaded', () => {
         entradaImagen.addEventListener('click', () => {
             fileInput.click();
         });
+
+        fileInput.addEventListener('change', () => {
+            const file = fileInput.files[0];
+            if (file) {
+                const allowedTypes = ['image/jpeg', 'image/png', 'image/gif', 'image/webp']; // Tipos de imagen permitidos
+                if (!allowedTypes.includes(file.type)) {
+                    alert("Por favor, suba un archivo de imagen válido (JPG, PNG, GIF, WEBP).");
+                    fileInput.value = ""; // Limpiar el input
+                    entradaImagen.childNodes[0].textContent= "Seleccionar Imagen"
+                } else {
+                     entradaImagen.childNodes[0].textContent = file.name; // Mostrar nombre del archivo seleccionado // Mostrar nombre del archivo seleccionado
+                }
+            }
+        });
     } else {
         console.error('Elemento(s) no encontrado(s) en el DOM');
     }
 });
+
